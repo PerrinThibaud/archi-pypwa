a57756bbbf36b70abc95e08e58656112ce63fd4d
Thibaud Perrin <thibaud.perrin6@gmail.com>
2022-10-30 21:29:25 +0100

    first commit

---
+++ static/python/client.py
@@ -0,0 +1,81 @@
+# noinspection PyUnresolvedReferences
+import random
+
+# noinspection PyUnresolvedReferences,PyPackageRequirements
+import pyodide
+# noinspection PyUnresolvedReferences,PyPackageRequirements
+from js import DOMParser, document, setInterval, console
+
+# noinspection PyPackages
+import subject_api
+
+# noinspection PyPackages
+from subject_api import Subject
+
+
+def main():
+    set_theme()
+    add_refresh_event()
+
+
+def set_theme():
+    body = document.getElementById('body')
+    div_content = document.getElementById('content')
+    div_name_architect = document.getElementById('name_architect')
+    div_quote_architect = document.getElementById('quote_architect')
+    div_architect = document.getElementById('architect')
+    div_architecture = document.getElementById('architecture')
+
+    add_class(div_content, 'hidden')
+    add_class(div_architect, 'hidden')
+
+    try:
+        forecast: Subject = subject_api.download_subject()
+    except Exception as x:
+        console.log("Error calling weather API: {}".format(x))
+        forecast = create_error_style_report()
+    body.style.setProperty("--main-color", forecast.main)
+    body.style.setProperty("--secondary-color", forecast.secondary)
+    div_name_architect.innerText = forecast.name
+    div_quote_architect.innerText = f"\"{forecast.quote}\""
+    div_architect.children[0].setAttribute('src', '../static/images/architects/{}.webp'.format(forecast.images))
+    div_architecture.children[0].setAttribute('src', '../static/images/architecture/{}.webp'.format(forecast.images))
+
+    remove_class(div_content, 'hidden')
+    remove_class(div_architect, 'hidden')
+
+
+def create_error_style_report():
+    forecast = Subject()
+    forecast.name = 'Offline'
+    forecast.quote = 'Quote API is offline.'
+    forecast.title = 'offline'
+    forecast.images = 'offline'
+    forecast.main = '#762635'
+    forecast.secondary = '#F3A6D0'
+    return forecast
+
+
+def add_refresh_event():
+    def evt(e=None):
+        set_theme()
+        if e:
+            e.preventDefault()
+        return False
+
+    refresh_link = document.getElementById('refresh')
+    refresh_link.onclick = evt
+
+
+def remove_class(element, class_name):
+    element.classList.remove(class_name)
+
+
+def add_class(element, class_name):
+    element.classList.add(class_name)
+
+
+try:
+    main()
+except Exception as x:
+    print("Error starting weather script: {}".format(x))
---
+++ static/python/subject_api.py
@@ -0,0 +1,73 @@
+import io
+import json
+import random
+
+# noinspection PyUnresolvedReferences
+import pyodide
+
+
+class Subject:
+    # noinspection PyDefaultArgument
+    def __init__(self, data={}):
+        self.name: str = data.get('name')
+        self.quote: str = data.get('quote')
+        self.title: str = data.get('title')
+        self.images: str = data.get('images')
+        self.main: str = data.get('main')
+        self.secondary: str = data.get('secondary')
+
+
+def get_random_subject() -> dict:
+    subjects = [
+        {
+            'name': 'Zaha hadid',
+            'quote': "I don't think that architecture is only about shelter, is only about a very simple enclosure.\
+                It should be able to excite you, to calm you, to make you think.",
+            'title': 'Heydar Aliyev Cultural Center',
+            'images': 'zh',
+            'main': '#1B365E',
+            'secondary': '#90B6DE',
+        },
+        {
+            'name': 'Bjarke Ingels',
+            'quote': "In the big picture, architecture is the art and science \
+                of making sure that our cities and buildings fit with the way we want to live our lives.",
+            'title': 'Kistefos Museum Norway',
+            'images': 'bi',
+            'main': '#97B5D9',
+            'secondary': '#01293D',
+        },
+        {
+            'name': 'Jean Nouvel',
+            'quote': "Architecture is always an answer given to a question that is not asked!",
+            'title': 'National Museum of Qatar',
+            'images': 'jn',
+            'main': '#795731',
+            'secondary': '#E5C489',
+        },
+        {
+            'name': 'Ieoh Ming Pei',
+            'quote': "Architecture is the very mirror of life. \
+                You only have to cast your eyes on buildings to feel the presence of the past, \
+                    the spirit of a place; they are the reflection of society.",
+            'title': 'The pyramid of Louvre',
+            'images': 'imp',
+            'main': '#1F305C',
+            'secondary': '#E6D7B0',
+        },
+        {
+            'name': 'Frank Gehry',
+            'quote': "Architecture should speak of its time and place, but yearn for timelessness.",
+            'title': 'La Tour - Luma Fondation',
+            'images': 'fg',
+            'main': '#4A2E1E',
+            'secondary': '#F2D2A7',
+        },
+    ]
+
+    return random.choice(subjects)
+
+def download_subject() -> Subject:
+    forecast: dict = get_random_subject()
+
+    return Subject(forecast)
